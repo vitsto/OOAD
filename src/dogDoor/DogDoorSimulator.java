6d9288b94197d49b606ea63ac73a1f0c5f65f301
vitsto <stolyarov.vitalij@list.ru>
2022-08-23 04:50:03 +0300

    dogDoors: timer improved

--- src/dogDoor/DogDoorSimulator.java
+++ src/dogDoor/DogDoorSimulator.java
@@ -7,9 +7,16 @@ public class DogDoorSimulator {
         System.out.println("Fido barks to go outside...");
         remote.pressButton();
         System.out.println("\nFido has gone outside...");
-
         System.out.println("\nFido's all done");
 
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException ignored) { }
+
+        System.out.println("... but he's stuck outside");
+        System.out.println("Fido starts barking...");
+        System.out.println("... so Gina grabs the remote control.");
+        remote.pressButton();
         System.out.println("\nFido's back inside");
 
     }
